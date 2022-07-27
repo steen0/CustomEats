cc4bfa1192121fb26ea63f095165345a0a223e59
tsteen <tsteen0731@gmail.com>
2022-07-27 14:16:59 -0400

    refactor initial user setup

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,86 @@
+const usersObject = [
+  {
+    username: 'user1',
+    password: 'baseball',
+  },
+  {
+    username: 'user2',
+    password: 'football',
+  },
+  {
+    username: 'admin',
+    password: 'password',
+  },
+];
+
+const ordersObject = [
+  {
+    id: '000001',
+    type: 'One-Time',
+    quantity: 1,
+    totalAmount: '$10.00',
+    createdDate: '07/03/2021',
+  },
+  {
+    id: '000002',
+    type: 'Recurring Monthly',
+    quantity: 10,
+    totalAmount: '$100.00',
+    createdDate: '02/09/2021',
+  },
+  {
+    id: '000003',
+    type: 'One-Time',
+    quantity: 5,
+    totalAmount: '$50.00',
+    createdDate: '01/22/2022',
+  },
+  {
+    id: '000004',
+    type: 'One-Time',
+    quantity: 3,
+    totalAmount: '$30.00',
+    createdDate: '08/05/2021',
+  },
+  {
+    id: '000005',
+    type: 'One-Time',
+    quantity: 1,
+    totalAmount: '$10.00',
+    createdDate: '05/19/2021',
+  },
+  {
+    id: '000006',
+    type: 'One-Time',
+    quantity: 25,
+    totalAmount: '$180.00',
+    createdDate: '04/09/2021',
+  },
+  {
+    id: '000007',
+    type: 'One-Time',
+    quantity: 1,
+    totalAmount: '$10.00',
+    createdDate: '05/19/2021',
+  },
+  {
+    id: '000008',
+    type: 'One-Time',
+    quantity: 25,
+    totalAmount: '$180.00',
+    createdDate: '04/09/2021',
+  },
+];
+
+// Create users object array if does not exist
+!localStorage.getItem('users')
+  ? localStorage.setItem('users', JSON.stringify(usersObject))
+  : null;
+
+!localStorage.getItem('orders')
+  ? localStorage.setItem('orders', JSON.stringify(ordersObject))
+  : null;
+
 class Login {
   constructor(form, ...fields) {
     this.form = form;
